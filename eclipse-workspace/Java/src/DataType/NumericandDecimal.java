3931bdc9649f8fb3db00b1169c00ecae37a18a58
Amit <amit.jagtap.nsk@gmail.com>
2023-07-30 18:10:54 +0530

    1st code

---
+++ eclipse-workspace/Java/src/DataType/NumericandDecimal.java
@@ -0,0 +1,48 @@
+package DataType;
+
+public class NumericandDecimal {
+	
+	public static void main (String []args) {
+		
+		//Starting point of the program
+		
+		//NumericandDecimal-Example- 33.45,4.6
+		
+		//1.Float-4 Byte-32 Bit
+		
+	// Variable dec and int
+		float F1=34.6f;
+		float F2=44;
+		float F3=444.567778f;
+		
+		float F4=-55.666f;
+		float F5=-555.66657f;
+		
+		System.out.println("Float Data type---"+F1);
+		System.out.println("Float data type---"+F2);
+		System.out.println("Float Data type---"+F3);
+		System.out.println("Float Data type---"+F4);
+		System.out.println();
+		System.out.println("*******************");
+		System.out.println("Flaot Data type---"+F5);
+		System.out.println("&&&&&&&&&&&&&&&&&7");
+		
+//We can store int value in float data type but we cannot store float in int data type
+	
+	//2.double-8 Byte-64 Bit
+	double d1=345;
+	double d2=555555.666666;
+	
+	double d3=-34567.332;
+	double d4=-4678.5;
+	
+	System.out.println("Double Data type---"+d1);
+	System.out.println("Double Data type----"+d2);
+	System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%");
+	System.out.println("Double Data type-----"+d3);
+	System.out.println("Double Data type-----"+d4);
+	}
+}
+
+//We can store int value in double data type but we cannot store double data type value in int data type
+
